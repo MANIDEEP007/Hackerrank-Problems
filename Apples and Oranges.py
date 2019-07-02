2ee52575c1bdf0b07e585cd3656fdd2b6c1be351
Paduchuri Manideep <paduchurimanideep@gmail.com>
2019-07-02 22:04:12 +0530

    Update Apples and Oranges.py

--- Apples and Oranges.py
+++ Apples and Oranges.py
@@ -1,5 +1,6 @@
 '''
-Sam’s house has an apple tree and an orange tree that yield an abundance of fruit. In the diagram below, the red region denotes his house, where s is the start point, and i is the endpoint. The apple tree is to the left of his house, and the orange tree is to its right. You can assume the trees are located on a single point, where the apple tree is at point a, and the orange tree is at point b. The link to problem is https://www.hackerrank.com/challenges/apple-and-orange
+Sam’s house has an apple tree and an orange tree that yield an abundance of fruit. In the diagram below, the red region denotes his house, where s is the start point, and i is the endpoint. The apple tree is to the left of his house, and the orange tree is to its right. You can assume the trees are located on a single point, where the apple tree is at point a, and the orange tree is at point b. 
+The link to problem is https://www.hackerrank.com/challenges/apple-and-orange
 '''
 import math
 import os
