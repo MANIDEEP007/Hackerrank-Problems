be59a27ec3849e5f70f6a9133dbc3ea6fae7fb22
Paduchuri Manideep <paduchurimanideep@gmail.com>
2019-07-02 22:55:00 +0530

    Update Apples and Oranges.py

--- Apples and Oranges.py
+++ Apples and Oranges.py
@@ -1,6 +1,26 @@
 '''
 Sam’s house has an apple tree and an orange tree that yield an abundance of fruit. In the diagram below, the red region denotes his house, where s is the start point, and i is the endpoint. The apple tree is to the left of his house, and the orange tree is to its right. You can assume the trees are located on a single point, where the apple tree is at point a, and the orange tree is at point b. 
 The link to problem is https://www.hackerrank.com/challenges/apple-and-orange
+
+Input Format
+
+The first line contains two space-separated integers denoting the respective values of  and . 
+The second line contains two space-separated integers denoting the respective values of  and . 
+The third line contains two space-separated integers denoting the respective values of  and . 
+The fourth line contains  space-separated integers denoting the respective distances that each apple falls from point . 
+The fifth line contains  space-separated integers denoting the respective distances that each orange falls from point .
+
+Sample Input 0
+
+7 11
+5 15
+3 2
+-2 2 1
+5 -6
+Sample Output 0
+
+1
+1
 '''
 import math
 import os
